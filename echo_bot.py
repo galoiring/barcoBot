e69749f0910d15d2d169bc4fbb093793d1571ea1
Gal Oiring <74841666+galoiring@users.noreply.github.com>
2022-12-01 13:03:06 +0200

    WIP echo bot

--- echo_bot.py
+++ echo_bot.py
@@ -1,3 +1,14 @@
 import telebot
 
 bot = telebot.TeleBot("5951730571:AAFJjGmdBUKse_a-si3n9Ia6yDDULroR0QM", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
