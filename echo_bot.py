93e29b4919d1754876fca5b6693b373759d19248
Gal Oiring <74841666+galoiring@users.noreply.github.com>
2022-12-01 18:03:46 +0200

    WIP trying to echo picture

--- echo_bot.py
+++ echo_bot.py
@@ -1,14 +1,29 @@
+import logging
+
 import telebot
 
-bot = telebot.TeleBot("5951730571:AAFJjGmdBUKse_a-si3n9Ia6yDDULroR0QM", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+bot_token = "5951730571:AAFJjGmdBUKse_a-si3n9Ia6yDDULroR0QM"
+bot = telebot.TeleBot(token=bot_token)
+
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Howdy, how are you doing?")
 
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
 
+@bot.message_handler(content_types=['photo'])
+def heandle_photos(message):
+    bot.reply_to(message, "This is a message handler")
+    bot.send_photo(message.chat.id, message.photo[0])
+
+
+# # Handles all sent photo files
+# @bot.message_handler(content_types=['photo'])
+# def handle_docs_audio(message):
+#     logging.warning(message)
+#
+#     bot.send_chat_action(message, 'typing')
+#     # bot.send_photo(message.id, message.photo)
+#
 
 bot.infinity_polling()
